8ee35a42f2d153e46ea2d33c97675ac4fa3e5ed0
MakerSquare Student <student@lab-2.att.net>
2013-06-21 10:58:28 -0500

    modify star.rb for activerecord lesson.

--- app/models/star.rb
+++ app/models/star.rb
@@ -1,3 +1,13 @@
 class Star < ActiveRecord::Base
   attr_accessible :birthdate, :city, :first_name, :last_name, :state
+	def full_name
+	  first_name + ' ' + last_name
+	end
+
+	def city_state
+		city + ", " + state
+	end
+
 end
+
+
